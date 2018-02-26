c9edef085721f8c8fe4fe931c901916de8db5e82
Max <mtayl062@uottawa.ca>
2018-02-25 21:46:46 -0500

    Add login functionality with session

--- login.php
+++ login.php
@@ -1,5 +1,39 @@
 <?php
-	
+	$conn = pg_connect('host=localhost port=5432 dbname=postgres user=postgres password=csi3540');
+	$username = $password = "";
+	$username_err = $password_err = "";
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty(trim($_POST["username"]))) {
+			$username_err = "Please enter username.";
+		} else {
+			$username = trim($_POST["username"]);
+		}
+		if (empty(trim($_POST["password"]))) {
+			$password_err = "Please enter password.";
+		} else {
+			$password = trim($_POST["password"]);
+		}
+		
+		if (empty($username_err) && empty($password_err)) {
+			$param_username = $username;
+			$query = sprintf("SELECT username, pwd FROM wbproj.users WHERE username='%s';",$username);
+			$result = pg_query($conn, $query);
+			if (pg_num_rows($result) == 1) {
+				$row = pg_fetch_row($result);
+				$stored_password = trim($row[1]);
+				$correct_login = password_verify($password,$stored_password);
+				if ($correct_login) {
+					session_start();
+					$_SESSION["username"] = $username;
+					header('location: index.php');
+				} else {
+					$password_err = "Password invalid.";
+				}
+			} else {
+				$username_err = "No such username.";
+			}
+		}
+	}
 ?>
 
 <!DOCTYPE html>
@@ -27,11 +61,16 @@
     </section>
 	
 	<section id="mainbox" class="w3-container w3-content w3-center w3-padding-large">
-		<form>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 			<img src="images/pet4.png" class="cute_img" alt="One of the game's pets"/>
-			<div><input type="text" class="input_margin" placeholder="username"></div>
-			<div><input type="password" class="input_margin" placeholder="password"></div>
-			<div><input type="submit" class="input_margin w3-button w3-purple"></div>
+			<div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+				<input type="text" class="input_margin form-control" placeholder="username" name="username" value="<?php echo $username; ?>">
+				<span class="help-block"><?php echo $username_err; ?></span></div>
+			<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+				<input type="password" class="input_margin form-control" placeholder="password" name="password">
+				<span class="help-block"><?php echo $password_err; ?></span></div>
+			<div><input type="submit" class="input_margin w3-button w3-purple">
+				</div>
 			<div class="input_margin">Not a member? Click <a href="signup.html">here</a> to sign up.</div>
 		</form>
 	</section>
