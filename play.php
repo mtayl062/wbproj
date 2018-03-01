5ea9304e03485777ba58398136ffd81557de2aba
Max <mtayl062@uottawa.ca>
2018-03-01 13:12:10 -0500

    Questions and levels generated in play.php

--- play.php
+++ play.php
@@ -1,15 +1,49 @@
 <?php
-	$lvl = null;
+	$level = null;
 	if (isset($_POST['lvl1'])) {
-		$lvl = 1;
+		$level = 1;
 	} elseif (isset($_POST['lvl2'])) {
-		$lvl = 2;
+		$level = 2;
 	} elseif (isset($_POST['lvl3'])) {
-		$lvl = 3;
+		$level = 3;
 	} elseif (isset($_POST['lvl4'])) {
-		$lvl = 4;
+		$level = 4;
 	}
 	$question = 1;
+	if (isset($_POST['qid'])) {
+		$question = intval($_POST['qid']);
+	}
+	$score = 0;
+	if (isset($_POST['score'])) {
+		$score = intval($_POST['score']);
+	}
+	$conn = pg_connect('host=localhost port=5432 dbname=postgres user=postgres password=csi3540');
+	$query = sprintf("select n1, d1, n2, d2, a, b, c, d, answer, op from wbproj.questions WHERE lid = '%d' AND qid = '%d'",$level,$question);
+	$result = pg_query($conn, $query);
+	$row = pg_fetch_row($result);
+	$n1 = $row[0];
+	$d1 = $row[1];
+	$n2 = $row[2];
+	$d2 = $row[3];
+	$a_choice = $row[4];
+	$b_choice = $row[5];
+	$c_choice = $row[6];
+	$d_choice = $row[7];
+	$ans = $row[8];
+	$op = $row[9];
+	$lans = null;
+	$rans = null;
+	$rans_style = null;
+	if ($op == "+") {
+		$lans = $n1*$d2;
+		$rans = $n2*$d1;
+		$rans_style = "rans";
+	} else {
+		$lans = $n1*$d2-$n2*$d1;
+		$rans = $n2*$d1;
+		$rans_style = "rans-minus";
+	}
+	$rest = $d1*$d2 - $rans - $lans;
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +56,7 @@
 	<link rel="stylesheet" href="style/main.css">
 	<link rel="stylesheet" href="style/play.css">
 	<script src="scripts/play.js"></script>
+	<script>window.onload = function(event) {prepareCSS(<?php echo $n1.','.$d1.','.$n2.','.$d2?>)};</script>
 </head>
 
 <body>
@@ -34,30 +69,39 @@
     </nav>
 	
 	<section id="main_header" class="w3-container w3-center">
-      <h1 class="w3-text-purple shadow"><b>Level 1</b></h1>
+      <h1 class="w3-text-purple shadow"><b>Level <?php echo $level?></b></h1>
     </section>
 	
 	<section id="mainbox" class="w3-container w3-content w3-center w3-padding-large">
+		<span id="scoreboard" class="w3-purple" >Your score: <?php echo $score?></span>
 		<img src="images/pet2.png" align="right"/>
-		<h3 class="w3-text-purple small_shadow"><b>Question 1:  </b><span id="f1">1/3</span> + <span id="f2">1/2</span> = ?</h3>
-			<ul>
-				<li class="rect l1"></li><li class="rect r1"></li><span id="plus"> + </span>
-				<li class="rect l2"></li><li class="rect r2"></li>
-			</ul>
+		<br>
+		<h2 class="w3-text-purple small_shadow"><b>Question <?php echo $question?> :  </b> <?php echo $n1.'/'.$d1?> <?php echo $op ?> <?php echo $n2.'/'.$d2?> = ?</h2>
+		<br>
+		<ul>
+			<li class="rect" id="l1"></li><li class="rect" id="r1"></li><span id="op"> <?php echo $op ?> </span>
+			<li class="rect" id="l2"></li><li class="rect" id="r2"></li>
+		</ul>
+		<br>
 		<div>
-			<button id="A" onclick="showAnswer(this.id);">A) 2/5</button>
-			<button id="B" onclick="showAnswer(this.id);">B) 4/5</button>
-			<button id="C" onclick="showAnswer(this.id);">C) 5/6</button>
-			<button id="D" onclick="showAnswer(this.id);">D) 4/6</button>
+			<button id="A" onclick="showAnswer(this.id,'<?php echo $ans?>');">A) <?php echo $a_choice?></button>
+			<button id="B" onclick="showAnswer(this.id,'<?php echo $ans?>');">B) <?php echo $b_choice?></button>
+			<button id="C" onclick="showAnswer(this.id,'<?php echo $ans?>');">C) <?php echo $c_choice?></button>
+			<button id="D" onclick="showAnswer(this.id,'<?php echo $ans?>');">D) <?php echo $d_choice?></button>
 		</div>
 		<div id="answer">
 			<h4 id="answer_text" class="w3-text-purple"></h4>
 			<ul id="answer_bar">
-				<li class="rect lans"></li><li class="rect lans"></li><li class="rect rans"></li><li class="rect rans"></li><li class="rect rans"></li><li class="rect rest"></li>
+				<?php for ($i = 0 ; $i < $lans; $i++) { echo '<li class="rect lans"></li>';} for ($i = 0 ; $i < $rans; $i++) { echo '<li class="rect '.$rans_style.'"></li>';} for ($i = 0 ; $i < $rest; $i++) { echo '<li class="rect rest"></li>';}?>
 			</ul>
 		<div>
 		<div>
-			<button id="next">Next</button>
+			<form method="POST" action="<?php if ($question == 10) {echo 'levelComplete.php';} else {echo 'play.php';}?>">
+				<input type="hidden" name="lvl<?php echo $level?>" value="lvl<?php echo $level?>">
+				<input type="hidden" name="qid" value="<?php echo($question + 1)?>">
+				<input type="hidden" name="score" id="score" value="<?php echo($score)?>">
+				<input type="submit" name="next" id="next" value="Next">
+			</form>
 		<div>
 	</section>
 	
--- scripts/play.js
+++ scripts/play.js
@@ -1,19 +1,34 @@
 var done = true;
 
-function showAnswer(id) {
+function showAnswer(id,ans) {
 	if (done == true) {
+		var correct = ans.toUpperCase();
 		var elem = document.getElementById("answer");
 		var text = document.getElementById("answer_text");
-		var right_button = document.getElementById("C");
+		var right_button = document.getElementById(correct);
 		right_button.style.backgroundColor = "Green";
-		if (id === "C") {
+		if (id === correct) {
 			text.innerHTML = "Correct!";
+			var scoreContainer = document.getElementById("score");
+			scoreContainer.value = parseInt(scoreContainer.value) + 10;
 		} else {
-			text.innerHTML = "Incorrect! The answer is 5/6.";
+			text.innerHTML = "Incorrect! The answer is " + correct + ".";
 			var wrong_button = document.getElementById(id);
 			wrong_button.style.backgroundColor = "Red";
 		}
 		elem.style.visibility = "visible";
 		done = false;
 	}
+}
+
+function prepareCSS(n1,d1,n2,d2) {
+	var boxes = d1*d2;
+	var l1 = document.getElementById("l1");
+	var r1 = document.getElementById("r1");
+	var l2 = document.getElementById("l2");
+	var r2 = document.getElementById("r2");
+	l1.style.width = n1*d2*30 + "px";
+	r1.style.width = d1*d2*30 - n1*d2*30 + "px";
+	l2.style.width = n2*d1*30 + "px";
+	r2.style.width = d1*d2*30 - n2*d1*30 + "px";
 }
