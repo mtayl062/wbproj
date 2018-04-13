51b48608114daa949cc9cda7eaa6dfaad945f860
Max <mtayl062@uottawa.ca>
2018-04-12 20:06:14 -0400

    remove presentation files

--- pres/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$pdo = null;
-	try {
-		$pdo = new PDO('pgsql:dbname=postgres;host=/cloudsql/wbproj-csi3540:us-central1:wbprojdb','postgres','postgres');
-	} catch (PDOException $e) {
-		echo $e->getMessage();
-	}
-	return $pdo;
-?>
--- pres/deploy.bat
+++
@@ -1,6 +0,0 @@
-call gsutil cp "C:\Users\maxou\Dropbox\Max\Winter 2018\CSI 3540 Structures et normes du web\Project\wbproj\sql_scripts\create_cloud.sql" gs://wbproj-csi3540-dump/
-call gsutil acl ch -u me7yaqszynenblqp5d36gozsyq@speckle-umbrella-pg-5.iam.gserviceaccount.com:W gs://wbproj-csi3540-dump/
-call gsutil acl ch -u me7yaqszynenblqp5d36gozsyq@speckle-umbrella-pg-5.iam.gserviceaccount.com:R gs://wbproj-csi3540-dump/create_cloud.sql
-call gcloud sql instances import wbprojdb gs://wbproj-csi3540-dump/create_cloud.sql --database postgres --quiet
-
-call gcloud app deploy --quiet
